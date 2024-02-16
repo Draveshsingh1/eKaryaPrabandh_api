e85ad0f80437ff709a591782e378d08c997426f0
Dravesh Singh <draveshsingh1@gmail.com>
2024-02-16 23:59:43 +0530

    intitial commit

---
+++ src/main/java/in/nic/eKaryaPrabandh/EKaryaPrabandhApplication.java
@@ -0,0 +1,13 @@
+package in.nic.eKaryaPrabandh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKaryaPrabandhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKaryaPrabandhApplication.class, args);
+	}
+
+}
